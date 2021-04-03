44971c4fe8509933e23625b54e5699c0224cc085
jacksoncl7 <jacksonlima@id.uff.br>
2021-04-03 15:00:49 -0300

    Init documentation branch
    Included doc comment in models/category.dart

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+//doc ok
 
 class Category{
+  // Modelo de objeto,
+  // define a entidade que sera utilizado majoriatariamente nas telas
+  // de categoria: category_list_page
   Color begin;
   Color end;
   String category;
   String image;
 
   Category(this.begin, this.end, this.category, this.image);
-
 
 }
